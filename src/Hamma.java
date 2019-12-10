ca1cfc37f2c57c12be9123655a1e1f4043199af2
user <nix_olympica@tut.by>
2019-12-10 18:57:57 +0300

    first hamma commit

--- src/Hamma.java
+++ src/Hamma.java
@@ -2,6 +2,12 @@
 public class Hamma {
 	Hamma() {
 		System.out.println("it is Hamma");
+		System.out.println("now we begin developing our element of the project");
+		method001();
+	}
+	
+	public void method001() {
+		System.out.println("Our first method: 001");
 	}
 
 }
---
+++ src/Hamma_two.java
@@ -0,0 +1,6 @@
+
+public class Hamma_two {
+	int simple_a = 0;
+	int simple_b = 500;
+
+}
